5f84d83a0eb12a0c5634acede9acf426c366a564
luiz <devluizfellipe@gmail.com>
2020-01-18 16:19:51 -0300

    adicionando rotas para edição e exclusão do dev

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,7 @@ const routes = Router();
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.get('/search',SearchController.index);
+routes.put('/update/:id', DevController.update);
+routes.delete('/delete/:id', DevController.destroy);
 
 module.exports = routes;
